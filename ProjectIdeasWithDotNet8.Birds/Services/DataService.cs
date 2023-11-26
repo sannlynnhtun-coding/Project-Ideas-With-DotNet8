7f06c0f9457232a223f9dbd9fd5d757cce6a8e7c
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2023-11-26 16:54:56 +0630

    -

--- ProjectIdeasWithDotNet8.Birds/Services/DataService.cs
+++ ProjectIdeasWithDotNet8.Birds/Services/DataService.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using ProjectIdeasWithDotNet8.Birds.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,15 +19,5 @@ namespace ProjectIdeasWithDotNet8.Birds.Services
         {
             return GetBirds().FirstOrDefault(x => x.Id == id)!;
         }
-    }
-
-
-    public class BirdModel
-    {
-        public int Id { get; set; }
-        public string BirdMyanmarName { get; set; }
-        public string BirdEnglishName { get; set; }
-        public string Description { get; set; }
-        public string ImagePath { get; set; }
     }
 }
